e8dcbd16cabef2305a774234a0b948167384bfe1
Dima <dimasgrand16@gmail.com>
2021-01-10 17:34:07 +0200

    111

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -8,13 +9,16 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
+
 let i = 0,
     nameOfMovie,
     ratingOfMovie;
+
 while(i < 2) {
     nameOfMovie = prompt("Один из последних просмотренных фильмов?", '');
     ratingOfMovie = prompt("На сколько оцените его?", '');
     personalMovieDB.movies[nameOfMovie] = ratingOfMovie;
     i++;
 }
+
 console.log(personalMovieDB);
